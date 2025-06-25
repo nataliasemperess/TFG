13d2777f8121fa66961bc0a5eca9656d37329f8d
nataliasemperess <al415537@uji.es>
2025-06-25 11:35:35 +0200

    sobra

--- simulación/TFG_ws/build/rmf_nayar/CMakeFiles/rmf_nayar_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rmf_nayar_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rmf_nayar_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
